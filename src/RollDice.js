f49f22d4aab5148300925802c34d964f290f9497
Tarek Fathy <Tarek.Fathy@Integrant.com>
2022-08-24 15:03:30 +0200

    Added Rolling State Modified button's behaviour

--- src/RollDice.js
+++ src/RollDice.js
@@ -4,28 +4,45 @@ import './RollDice.css';
 
 class RollDice extends Component {
     static defaultProps = {
-        sides: ["one", "two", "three", "four", "five", "six"] 
+        sides: ["one", "two", "three", "four", "five", "six"]
     };
     constructor(props) {
         super(props);
         this.state = {
             die1: "one",
-            die2: "one"
+            die2: "one",
+            isRolling: false
         }
         this.roll = this.roll.bind(this)
     }
-    roll(){
+    roll() {
         const dieRoll1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
         const dieRoll2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
-        this.setState({die1 : dieRoll1, die2 : dieRoll2});
+        this.setState({
+            die1: dieRoll1,
+            die2: dieRoll2,
+            isRolling: true
+        });
+
+        setTimeout(() => {
+            this.setState({isRolling: false})
+        }, 1000);
     }
     render() {
+        const { die1, die2, isRolling } = this.state;
+
         return <div className='RollDice'>
             <div className='RollDice-container'>
-            <Die face = {this.state.die1} />
-            <Die face = {this.state.die2} />
+                <Die face={die1} />
+                <Die face={die2} />
             </div>
-            <button onClick = {this.roll}>Roll Dice!</button>
+            <button onClick={this.roll} disabled = {isRolling}>
+                {
+                    isRolling ?
+                        `Rolling...` :
+                        `Roll Dice!`
+                }
+            </button>
         </div>
     }
 }
